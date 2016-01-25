fdc66f5d019478424bf9c8befc940858d5e650f7
Heath Chiavettone <hchiavettone@guidewire.com>
2016-01-25 12:10:56 -0800

    ORION-11: Implemented the metadata driven TableList component, using vehicles as the first test case

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,11 +1,85 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import Parent from './Parent';
+import TableList from './TableList';
 
 export default class App extends React.Component {
   render () {
+    const vehicles = [
+      {
+        "id": 100,
+        "policyId": 1,
+        "type": "automobile",
+        "color": "blue",
+        "make": "Honda",
+        "model": "Civic",
+        "year": 1996,
+        "vIN": "9",
+        "plate": "4CAR400",
+        "state": "CA"
+      },
+      {
+        "id": 101,
+        "policyId": 1,
+        "type": "automobile",
+        "color": "red",
+        "make": "Honda",
+        "model": "Accord",
+        "year": 1997,
+        "vIN": "11",
+        "plate": "4FUD450",
+        "state": "CA"
+      }
+    ];
+    const vehicleMetaData = {
+      "caption": "Select Vehicles from Policy",
+      "rowKeyId": "id",
+      "tableClass": "vehicles",
+      "cells": [
+        {
+          "name": "",
+          "field": "",
+          "columnClass": ""
+        },
+        {
+          "name": "Year",
+          "field": "year",
+          "columnClass": ""
+        },
+        {
+          "name": "Make",
+          "field": "make",
+          "columnClass": ""
+        },
+        {
+          "name": "Model",
+          "field": "model",
+          "columnClass": ""
+        },
+        {
+          "name": "License #",
+          "field": "plate",
+          "columnClass": ""
+        },
+        {
+          "name": "State",
+          "field": "state",
+          "columnClass": ""
+        },
+        {
+          "name": "",
+          "field": "",
+          "columnClass": ""
+        },
+        {
+          "name": "",
+          "field": "",
+          "columnClass": "editing"
+        }
+      ]
+    };
+
     return (
-        <Parent />
+        <TableList tableData={vehicles} metaData={vehicleMetaData}/>
     );
   }
 }
